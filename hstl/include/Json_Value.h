8017c46099fc899972f552cbecc98768dba239a0
Mohamed Hashim <48164891+MHSHM@users.noreply.github.com>
2025-06-04 14:18:09 +0300

    Playing with Json_Value

---
+++ hstl/include/Json_Value.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace hstl
+{
+	class Json_Value
+	{
+	
+	};
+};
--- playground/src/main.cpp
+++ playground/src/main.cpp
@@ -1,8 +1,70 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <cstdint>
+#include <assert.h>
+
+class Json_Value
+{
+
+private:
+	enum VALUE_TYPE
+	{
+		BOOL,
+		DOUBLE,
+		STRING,
+		ARRAY,
+		OBJECT,
+		EMPTY
+	};
+
+	union Value
+	{
+		Value() { }
+		~Value() {}
+
+		bool b;
+		double d;
+		std::string s;
+		std::vector<Json_Value> array;
+		std::unordered_map<std::string, Json_Value> object;
+	};
+
+	Value value;
+	VALUE_TYPE active_value_type;
+
+public:
+	Json_Value():
+		active_value_type(VALUE_TYPE::EMPTY)
+	{
+	
+	}
+
+	Json_Value(const bool& b):
+		active_value_type(VALUE_TYPE::BOOL)
+	{
+		value.b = b;
+	}
+
+	Json_Value(const double& d):
+		active_value_type(VALUE_TYPE::DOUBLE)
+	{
+		value.d = d;
+	}
+
+	Json_Value(const std::string& s):
+		active_value_type(VALUE_TYPE::STRING)
+	{
+		new (&value.s) std::string(s);
+	}
+
+	~Json_Value()
+	{
+	}
+};
 
 int main()
 {
-	std::cout << "Hello HSTL\n";
 
 	return 0;
 }
