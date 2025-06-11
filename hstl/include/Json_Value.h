5b79b440ddb8793e612efedcf5c7d13a1dd30e40
Mohamed Hashim <48164891+MHSHM@users.noreply.github.com>
2025-06-11 14:23:33 +0300

    Json Value

--- hstl/include/Json_Value.h
+++ hstl/include/Json_Value.h
@@ -1,9 +1,56 @@
 #pragma once
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <initializer_list>
 
 namespace hstl
 {
 	class Json_Value
 	{
-	
+	private:
+		enum VALUE_TYPE
+		{
+			BOOL,
+			DOUBLE,
+			STRING,
+			ARRAY,
+			OBJECT,
+			EMPTY
+		};
+
+		union Value
+		{
+			Value() {}
+			~Value() {}
+			Value(const Value&) = delete;
+			Value& operator=(const Value&) = delete;
+
+			bool b;
+			double d;
+			std::string s;
+			std::vector<Json_Value> array;
+			std::unordered_map<std::string, Json_Value> object;
+		};
+
+		Value       value;
+		VALUE_TYPE  active_value_type;
+
+	public:
+		Json_Value();
+		Json_Value(const Json_Value& src);
+		Json_Value& operator=(const Json_Value& src);
+		~Json_Value();
+
+		Json_Value(const bool& b);
+		Json_Value(const double& d);
+		Json_Value(const std::string& s);
+		Json_Value(const char* s);
+
+		Json_Value(const std::vector<Json_Value>& array);
+		Json_Value(const std::initializer_list<Json_Value>& il);
+
+		Json_Value(const std::unordered_map<std::string, Json_Value>& object);
+		Json_Value(const std::initializer_list<std::pair<std::string, Json_Value>>& il);
 	};
-};
+} // namespace hstl
---
+++ hstl/src/Json_Value.cpp
@@ -0,0 +1,115 @@
+#include "Json_Value.h"
+#include <new>
+
+using namespace hstl;
+
+Json_Value::Json_Value()
+	:active_value_type(EMPTY) {}
+
+Json_Value::~Json_Value()
+{
+	switch (active_value_type)
+	{
+	case STRING: value.s.~basic_string(); break;
+	case ARRAY:  value.array.~vector();   break;
+	case OBJECT: value.object.~unordered_map(); break;
+	default: break;                       // BOOL, DOUBLE, EMPTY
+	}
+}
+
+Json_Value::Json_Value(const Json_Value& src)
+	:active_value_type(EMPTY)
+{
+	switch (src.active_value_type)
+	{
+	case BOOL:   value.b = src.value.b; break;
+	case DOUBLE: value.d = src.value.d; break;
+	case STRING: new (&value.s) std::string(src.value.s); break;
+	case ARRAY:  new (&value.array) std::vector<Json_Value>(src.value.array); break;
+	case OBJECT: new (&value.object) std::unordered_map<std::string, Json_Value>(src.value.object); break;
+	case EMPTY:
+	default: break;
+	}
+	active_value_type = src.active_value_type;
+}
+
+Json_Value& Json_Value::operator=(const Json_Value& src)
+{
+	if (this == &src)
+		return *this;
+
+	// destroy current payload (if any)
+	if (active_value_type != EMPTY)
+	{
+		switch (active_value_type)
+		{
+		case STRING: value.s.~basic_string(); break;
+		case ARRAY:  value.array.~vector();   break;
+		case OBJECT: value.object.~unordered_map(); break;
+		default: break;
+		}
+		active_value_type = EMPTY;
+	}
+
+	// copy from source
+	switch (src.active_value_type)
+	{
+	case BOOL:   value.b = src.value.b; break;
+	case DOUBLE: value.d = src.value.d; break;
+	case STRING: new (&value.s) std::string(src.value.s); break;
+	case ARRAY:  new (&value.array) std::vector<Json_Value>(src.value.array); break;
+	case OBJECT: new (&value.object) std::unordered_map<std::string, Json_Value>(src.value.object); break;
+	case EMPTY:
+	default: break;
+	}
+	active_value_type = src.active_value_type;
+	return *this;
+}
+
+Json_Value::Json_Value(const bool& b)
+	:active_value_type(BOOL)
+{
+	value.b = b;
+}
+
+Json_Value::Json_Value(const double& d)
+	:active_value_type(DOUBLE)
+{
+	value.d = d;
+}
+
+Json_Value::Json_Value(const std::string& s)
+	:active_value_type(STRING)
+{
+	new (&value.s) std::string(s);
+}
+
+Json_Value::Json_Value(const char* s)
+	:active_value_type(STRING)
+{
+	new (&value.s) std::string(s);
+}
+
+Json_Value::Json_Value(const std::vector<Json_Value>& array)
+	:active_value_type(ARRAY)
+{
+	new (&value.array) std::vector<Json_Value>(array);
+}
+
+Json_Value::Json_Value(const std::initializer_list<Json_Value>& il)
+	:active_value_type(ARRAY)
+{
+	new (&value.array) std::vector<Json_Value>(il);
+}
+
+Json_Value::Json_Value(const std::unordered_map<std::string, Json_Value>& object)
+	:active_value_type(OBJECT)
+{
+	new (&value.object) std::unordered_map<std::string, Json_Value>(object);
+}
+
+Json_Value::Json_Value(const std::initializer_list<std::pair<std::string, Json_Value>>& il)
+	:active_value_type(OBJECT)
+{
+	new (&value.object) std::unordered_map<std::string, Json_Value>(il.begin(), il.end());
+}
