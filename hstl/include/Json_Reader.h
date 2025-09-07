abb0de53df75083db4570b829253ac6638dbc38c
Mohamed Hashim <48164891+MHSHM@users.noreply.github.com>
2025-09-07 23:35:33 +0300

    Initial foundation for the Json_Reader

--- hstl/include/Json_Reader.h
+++ hstl/include/Json_Reader.h
@@ -1,6 +1,35 @@
 #pragma once
 
+#include <vector>
+#include <string>
+
 namespace hstl
 {
+	enum class TOKEN_TYPE
+	{
 	
+	};
+
+	class Token
+	{
+	public:
+
+	private:
+		TOKEN_TYPE type;
+		struct
+		{
+			uint32_t line;
+			uint32_t byte_offset;
+			uint32_t column;
+		} loc;
+		std::string payload;
+	};
+
+	class Lexer
+	{
+	public:
+
+	private:
+		std::vector<Token> tokens;
+	};
 };
