5c128bf5ee25bbdc04ca930db3e6ba97781b487c
Mohamed Hashim <48164891+MHSHM@users.noreply.github.com>
2025-11-16 17:07:36 +0200

    Initial skeleton for Array.h

---
+++ hstl/include/Array.h
@@ -0,0 +1,35 @@
+#pragma once
+
+namespace hstl
+{
+	class Array
+	{
+	public:
+		Array() = default;
+		Array(size_t capacity) {  };
+		Array(size_t size) { };
+		Array(size_t size, int value) { }
+		Array(const Array& source) { }
+		Array& operator=(const Array& source) { }
+		Array(Array&& source) { }
+		Array& operator=(Array&& source) { }
+		~Array() { }
+
+	public:
+		bool reserve(size_t capacity) {}
+		bool resize(size_t size) { }
+		bool resize_with_value(size_t size, int value) { }
+		bool push(int value) { }
+		bool shrink_to_fit() { }
+		bool remove(size_t index) { }
+		bool remove_ordered(size_t index) { }
+		void remove_if() { }
+		int* begin() { }
+		int* end() { }
+
+	private:
+		int* data;
+		size_t size;
+		size_t capacity;
+	};
+};
