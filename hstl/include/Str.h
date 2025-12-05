dc8db2add0d9fcd73ec8c22bd2380249ce708d52
Mohamed Hashim <48164891+MHSHM@users.noreply.github.com>
2025-12-05 19:42:55 +0200

    More working on Str, involves adding a Str_View and some more functionality to Str

--- hstl/include/Str.h
+++ hstl/include/Str.h
@@ -4,6 +4,50 @@
 
 namespace hstl
 {
+	// Non owning wrapper aound a sequence of charachters
+	class Str_View
+	{
+	public:
+		Str_View(const char* _data, size_t _count):
+			_data{_data},
+			_count{_count}
+		{
+
+		}
+
+	public:
+		size_t count() const
+		{
+			return _count;
+		}
+
+		const char* data() const
+		{
+			return _data;
+		}
+
+		const char& operator[](size_t index) const
+		{
+			if (index >= _count)
+			{
+				throw std::out_of_range{"Index is out of range"};
+			}
+
+			return _data[index];
+		}
+
+		size_t find(const char* substr) const
+		{
+
+		}
+
+		// TODO: Implement useful functionality
+
+	private:
+		const char* _data;
+		size_t _count;
+	};
+
 	class Str
 	{
 	public:
@@ -25,7 +69,7 @@ namespace hstl
 
 			data.resize(string_len + 1);
 
-			memcpy(data.buffer(), c_str, string_len + 1);
+			memcpy(data.data, c_str, string_len + 1);
 		}
 
 		Str(char ch, size_t count)
@@ -38,7 +82,7 @@ namespace hstl
 
 			data.resize(count + 1);
 
-			memset(data.buffer(), ch, sizeof(char) * count);
+			memset(data.data, ch, sizeof(char) * count);
 
 			data[count] = '\0';
 		}
@@ -54,11 +98,57 @@ namespace hstl
 		{
 			data.push(ch);
 
-			auto count = data.size();
+			auto count = data.count;
 
 			std::swap(data[count - 1], data[count - 2]);
 
 			return data[count - 2];
+		}
+
+		Str_View push(const char* str)
+		{
+			size_t count = strlen(str);
+			size_t old_count = data.count;
+			size_t needed = old_count + count;
+
+			if (needed > data.capacity)
+			{
+				data.reserve(needed * 3u);
+			}
+
+			memcpy(data.data + (old_count - 1), str, sizeof(char) * count);
+
+			data.count += count;
+
+			data[data.count - 1] = '\0';
+
+			return Str_View{data.data + old_count - 1u, count};
+		}
+
+		void resize(size_t count, char ch)
+		{
+			size_t old_count = data.count;
+			size_t new_count = count + 1;
+
+			if (count == 0u)
+			{
+				init_empty_string();
+				return;
+			}
+
+			data.resize(new_count);
+
+			if (new_count > old_count)
+			{
+				memset(data.data + (old_count - 1), ch, sizeof(char) * (new_count - old_count));
+			}
+
+			data[count] = '\0';
+		}
+
+		void reserve(size_t capacity)
+		{
+			data.reserve(capacity);
 		}
 
 		const char* begin() const
@@ -83,7 +173,25 @@ namespace hstl
 
 		size_t count() const
 		{
-			return data.size() - 1;
+			return data.count - 1;
+		}
+
+		const char* c_str() const
+		{
+			return data.data;
+		}
+
+		void clear()
+		{
+			data.count = 1u;
+			data[0] = '\0';
+		}
+
+		// Will make a read-only view of the string, any modifications to the source string
+		// can cause the view to be invalid
+		Str_View view()
+		{
+			return Str_View{data.data, data.count - 1u};
 		}
 
 	private:
