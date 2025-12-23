e47646dc9aa95b8afe5cfa09b16c750528e86c41
Mohamed Hashim <48164891+MHSHM@users.noreply.github.com>
2025-12-23 20:56:30 +0200

    Rework Result class

--- hstl/include/Result.h
+++ hstl/include/Result.h
@@ -1,52 +1,115 @@
 #pragma once
 
-#include <string>
-#include <optional>
+#include "Str.h"
+
+#include <memory>
 #include <assert.h>
 
 namespace hstl
 {
 	class Err
 	{
-	public:
-		Err() = delete;
-		explicit Err(const std::string& _message):
-			message(_message) {}
-
-		const std::string& get_message() const { return message; }
-
 	private:
-		std::string message;
+		Str_View message;
+
+	public:
+		Err(const char* message):
+			message{message}
+		{
+
+		}
+
+		Str_View get_message() const
+		{
+			return message;
+		}
 	};
 
 	template<typename T>
 	class Result
 	{
+	private:
+		union
+		{
+			Err err;
+			T res;
+		};
+
+		bool has_err{false};
+
 	public:
 		Result(const Err& _err):
-			err(_err),
-			value(std::nullopt) {}
-
-		Result(const T& _value):
-			value(_value),
-			err(std::nullopt) {}
-
-		operator bool() const {return !err.has_value();}
-
-		const std::string& get_error() const
+			has_err{true}
 		{
-			assert(err.has_value() && "The result is successful, it has no error attached");
-			return (*err).get_message();
+			new (&err) Err(_err);
 		}
 
-		const T& get_value() const
+		Result(const T& t):
+			has_err{false}
 		{
-			assert(!err.has_value() && "The result isn't successful, it has no valid value");
-			return *value;
+			new (&res) T(t);
 		}
 
-	private:
-		std::optional<Err> err;
-		std::optional<T> value;
+		Result(T&& t):
+			has_err{false}
+		{
+			new (&res) T(std::move(t));
+		}
+
+		Result(const Result& other):
+			has_err{other.has_err}
+		{
+			if (other.has_err)
+			{
+				new (&err) Err(other.err);
+			}
+			else
+			{
+				new (&res) T(other.res);
+			}
+		}
+
+		Result(Result&& other):
+			has_err{other.has_err}
+		{
+			if (other.has_err)
+			{
+				new (&err) Err(std::move(other.err));
+			}
+			else
+			{
+				new (&res) T(std::move(other.res));
+			}
+		}
+
+		operator bool() const
+		{
+			return !has_err;
+		}
+
+		Result& operator=(const Result&) = delete;
+		Result& operator=(Result&&) = delete;
+
+		T& get_value()
+		{
+			assert(has_err == false);
+
+			return res;
+		}
+
+		Str_View get_err() const
+		{
+			assert(has_err == true);
+
+			return err.get_message();
+		}
+
+		~Result()
+		{
+			if (has_err == false)
+			{
+				std::destroy_at(&res);
+			}
+		}
 	};
 };
